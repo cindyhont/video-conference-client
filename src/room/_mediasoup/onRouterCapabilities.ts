c5d885ac493e42de21e8b25e39de74ddf791d59f
cindyhont <cindyhont@gmail.com>
2023-01-24 14:29:00 +0000

    ios firefox error known, cannot fix

--- src/room/_mediasoup/onRouterCapabilities.ts
+++ src/room/_mediasoup/onRouterCapabilities.ts
@@ -11,50 +11,11 @@ const onRouterCapabilities = async (routerRtpCapabilities:RtpCapabilities) => {
     try {
         const _device = new Device()
         setDevice(_device)
+        await device.load({ routerRtpCapabilities })
     } catch (error) {
-        // show firefox error
-        const p = document.createElement('p')
-        p.innerText = error
-        p.style.color = '#fff'
-        p.style.position = 'absolute'
-        p.style.top = '0'
-        p.style.left = '0'
-        p.style.zIndex = '1000'
-        p.style.marginBottom = '20px'
-        p.style.backgroundColor = '#000'
-        document.body.appendChild(p)
-
-
         enterRoomContainer.classList.add('hidden')
         showMsgBox(browserNotSupportMsg)
         if (error.name === 'UnsupportedError') console.error('Browser not supported')
-        const message:IdeleteClient = {
-            type:'deleteClient',
-            payload:{clientID}
-        }
-        send(message,websocket)
-        return
-    }
-
-    try {
-        await device.load({ routerRtpCapabilities })
-    } catch (error) {
-        // show firefox error
-        const p = document.createElement('p')
-        p.innerText = error
-        p.style.color = '#fff'
-        p.style.position = 'absolute'
-        p.style.top = '0'
-        p.style.left = '0'
-        p.style.zIndex = '1000'
-        p.style.backgroundColor = '#000'
-        document.body.appendChild(p)
-
-
-
-
-        enterRoomContainer.classList.add('hidden')
-        showMsgBox(browserNotSupportMsg)
         const message:IdeleteClient = {
             type:'deleteClient',
             payload:{clientID}
